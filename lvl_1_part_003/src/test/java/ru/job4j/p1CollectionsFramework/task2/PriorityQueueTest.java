073ac6bda67c4aaf1679d2d1ccb9a09d081456d0
Alexgta <9720560@gmail.com>
2018-11-21 21:06:45 +0300

    Удаление лишнего2 в (2. Очередь с приоритетом на LinkedList [#41670][#90878])

--- lvl_1_part_003/src/test/java/ru/job4j/p1CollectionsFramework/task2/PriorityQueueTest.java
+++ lvl_1_part_003/src/test/java/ru/job4j/p1CollectionsFramework/task2/PriorityQueueTest.java
@@ -15,7 +15,6 @@ public class PriorityQueueTest {
       queue.put(new Task("low", 5));
       queue.put(new Task("urgent", 1));
       queue.put(new Task("middle", 3));
-      int queueSize = queue.getTasksSize();
       Task result = queue.take();
       assertThat(result.getDesc(), is("urgent"));
    }
