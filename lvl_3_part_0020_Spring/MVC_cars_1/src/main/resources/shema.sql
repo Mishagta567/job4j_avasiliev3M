14bf34f5f812a2128f79fde714e33c61859f4481
Alexgta <9720560@gmail.com>
2018-11-27 20:48:51 +0300

    Мелочи

--- lvl_3_part_0020_Spring/MVC_cars_1/src/main/resources/shema.sql
+++ lvl_3_part_0020_Spring/MVC_cars_1/src/main/resources/shema.sql
@@ -4,12 +4,12 @@ CREATE TABLE public.users
    login character varying(50),
    password character varying(50),
    name character varying(200),
-   email character varying(200),
-   inserted_date TIMESTAMP
+   email character varying(200)
+   --, inserted_date TIMESTAMP
 );
-INSERT INTO users (login, password, name, inserted_date, email) VALUES('alex', '123', 'Alex', CURRENT_TIMESTAMP(0), 'alex@mail.ru');
-INSERT INTO users (login, password, name, inserted_date, email) VALUES('agent007', '123', 'Agent 007', CURRENT_TIMESTAMP(0), '007@mail.ru');
-INSERT INTO users (login, password, name, inserted_date, email) VALUES('agent009', '123', 'Agent KGB', CURRENT_TIMESTAMP(0), 'kgb@mail.ru');
+INSERT INTO users (login, password, name, email) VALUES('alexandr', '123', 'Alex',  'alex@mail.ru'); -- CURRENT_TIMESTAMP(0),
+INSERT INTO users (login, password, name, email) VALUES('agent007', '123', 'Agent 007', '007@mail.ru');
+INSERT INTO users (login, password, name, email) VALUES('agent009', '123', 'Agent KGB', 'kgb@mail.ru');
 
 
 CREATE TABLE cars_brand (
